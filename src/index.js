bae4723a33b7c51210224a3df0b8723fe42aaec5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-24 18:21:45 -0600

    Lecture 91: Resource Reading Endpoints - Part I

--- src/index.js
+++ src/index.js
@@ -16,6 +16,47 @@ const port = process.env.PORT || 3000;
 // parse all request/response body to JSON
 app.use(express.json());
 
+// GET /users: fetch all users
+app.get('/users', (req, res) => {
+
+    // User.find() to make a query on the 'users' document
+    // empty query object forces to fetch all documents from database
+    // 500: Internal Server Error
+    User
+        .find({})
+        .then(users => res.status(200).send(users))
+        .catch(err => res.status(500).send(err));
+
+
+});
+
+// GET /users/:id: fetch a user by its id
+// :id allows to catch the 'id' dynamic path variable
+app.get('/users/:id', (req, res) => {
+
+    // req.params stores all path variables from the incoming route
+    // so we access the id from it
+    const _id = req.params.id;
+
+    // use findById() to directly inject the _id path variable and make the query
+    // Mongoose automatically converts the String _id into its raw binary form
+    User
+        .findById(_id)
+        .then(user => {
+
+            // if a user was not found (null), send a 404 NOT FOUND
+            if (!user) {
+                return res.status(404).send('The user was not found');
+            }
+
+            // if it was found, send a 200 OK with the user object found
+            res.status(200).send(user);
+
+        })
+        .catch(err => res.status(500).send(err));
+
+});
+
 // POST /users: create a new user
 app.post('/users', (req, res) => {
 
