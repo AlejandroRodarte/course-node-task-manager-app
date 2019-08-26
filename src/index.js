ee6f7657cb7145e758942b2dcc3acb536ad43d41
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-25 18:53:00 -0600

    Lecture 108: Express Middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// use express
 const express = require('express');
 
 // run mongoose.js file to connect mongoose to MongoDB database
@@ -14,6 +15,11 @@ const app = express();
 
 // use the Heroku assigned port, if not, port 3000 for development
 const port = process.env.PORT || 3000;
+
+// register middleware also with app.use()
+app.use((req, res, next) => {
+    res.status(503).send('Server is on maintenance. Please come back in around 3 hours.');
+});
 
 // parse all request/response body to JSON
 app.use(express.json());
