07845590993b4fba6684e3c7809d2db7532034d7
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-27 22:07:18 -0600

    Lecture 140: Testing Asynchronous Code

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math');
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius, add } = require('../src/math');
 
 // test to check calculateTip() works
 test('Should return the total with the tip included.', () => {
@@ -32,4 +32,27 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
     const farenheit = celsiusToFahrenheit(0);
     expect(farenheit).toBe(32);
+});
+
+// use the done() function argument to let Jest know when all async tasks are done
+test('Async test demo', (done) => {
+    setTimeout(() => {
+        expect(1).toBe(2);
+        done();
+    }, 2000);
+});
+
+// place expect() inside the then() to run it after the Promise has been resolved/rejected
+test('Should add two numbers', (done) => {
+    add(2, 3)
+        .then(sum => {
+            expect(sum).toBe(5);
+            done();
+        });
+});
+
+// use async/await since Jest will actually wait for the Promises to be resolved before running expect()
+test('Should add two numbers with async/await', async () => {
+    const sum = await add(2, 3);
+    expect(sum).toBe(6);
 });
