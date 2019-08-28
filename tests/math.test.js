988f249dcd0a2bfff2105e6370596e7ea6c9d8ef
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-27 21:41:54 -0600

    Lecture 139: Writing your Own Tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,23 @@
-test('Hello world!', () => {
+const { calculateTip } = require('../src/math');
+
+// test to check calculateTip() works
+test('Should return the total with the tip included.', () => {
+
+    // call the function and store its return value
+    const total = calculateTip(10, 0.3);
+
+    expect(total).toBe(13);
+
+    // compare with the real, theoretical result
+    // if they don't match, throw an Error so the test fails
+    // if (total !== 13) {
+    //     throw new Error(`Wrong result. Expected tip: 13. Returned tip: ${total}`);
+    // }
 
 });
 
-test('This should fail', () => {
-    throw new Error('Failure!');
+// test the case where we use the default tip percentage
+test('Should calculate total with default tip.', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12.50);
 });
